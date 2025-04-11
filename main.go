9dcdce76bde1c0f17a2e198f27d225f765cbfdcb
venticx <venticx@gmail.com>
2025-04-11 10:54:14 +0200

    modified main.go to accept command line arguments

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/sgroez/huawei-e3372-sms-tui/api"
 )
@@ -11,11 +14,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	slr, err := api.GetSmsList(instance)
-	if err != nil {
-		panic(err)
+	args := os.Args[1:]
+	if len(args) <= 0 {
+		panic(errors.New("Please pass in the method as command line argument!"))
 	}
-	fmt.Println(slr)
-	//api.DeleteSms(instance, id)
-	//api.SendSms(instance, []string{"number"}, "test message from golang land")
+	switch(args[0]) {
+	case "list_sms":
+		slr, err := api.GetSmsList(instance)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(slr)
+	case "delete_sms":
+		if len(args) <= 1 {
+			panic(errors.New("Please pass in the id of the message to delete when choosing the delete_sms method!"))
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			panic(err)
+		}
+		api.DeleteSms(instance, id)
+	case "send_sms":
+		if len(args) <= 2 {
+			panic(errors.New("Please pass in at least on phone number and one message in that order when choosing the send_sms method!"))
+		}
+		phones := args[1:len(args) -1]
+		message := args[len(args) -1]
+		api.SendSms(instance, phones, message)
+	}
 }
